440557013783bc515a76eda024d2a7ea358daf4d
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-06-03 17:59:56 -0300

    configure linter/prettier

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,6 +10,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen flex flex-col">
       <Nav />
       <main className="grow flex flex-col">{children}</main>
+
       <Footer />
     </div>
   );
