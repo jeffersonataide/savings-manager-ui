c485a94572e405cdb3e3e544349967f4d57da933
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-10 16:46:57 -0300

    use authorization header for api requests

--- src/services/api/index.tsx
+++ src/services/api/index.tsx
@@ -2,6 +2,15 @@ import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+const getJWT = () => {
+  return localStorage.getItem("jwt");
+};
+
+const jwt = getJWT();
+
 export const api = axios.create({
   baseURL: API_URL,
+  headers: {
+    Authorization: jwt ? `Bearer ${jwt}` : "",
+  },
 });
