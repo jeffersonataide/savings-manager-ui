0a03ff04c866566b06aba8aaad1f38dc83782bcd
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-14 13:58:05 -0300

    move portfolio creation to portfolios page

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -2,8 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { CreateAccount } from "../../page/CreateAccount";
 import { Home } from "../../page/Home";
 import { Login } from "../../page/Login";
+import { Portfolios } from "../../page/Portfolios";
 import { Layout } from "../Layout";
-import { PortfoliosList } from "../PortfoliosList";
 
 export const Router = () => {
   return (
@@ -13,7 +13,7 @@ export const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/assets" element={<p>assets page</p>} />
-        <Route path="/portfolio" element={<PortfoliosList />} />
+        <Route path="/portfolio" element={<Portfolios />} />
       </Routes>
     </Layout>
   );
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,29 +1,7 @@
-import { CreatePortfolioForm } from "../../components/CreatePortfolioForm";
-import { PortfoliosList } from "../../components/PortfoliosList";
-import { useModal } from "../../contexts/modalContext";
-
 export const Home = () => {
-  const modalContext = useModal();
-
-  const openModal = () => {
-    modalContext.openModal({
-      title: "Create Portfolio",
-      content: <CreatePortfolioForm onSubmit={modalContext.closeModal} />,
-    });
-  };
-
   return (
     <div className="flex flex-col items-center">
-      <div className="my-4">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-black bg-opacity-70 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-60 "
-        >
-          Create portfolio
-        </button>
-      </div>
-      <PortfoliosList />
+      <h1 className="text-lg">Create an account to use the application</h1>
     </div>
   );
 };
