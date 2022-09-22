e0b2c543dc84da7709fc034dae9f567584631fd1
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-22 11:52:57 -0300

    create DeleteItemForm component

---
+++ src/components/Atomic/Molecules/DeleteItemForm/index.tsx
@@ -0,0 +1,32 @@
+interface DeleteItemFormParams {
+  itemName: string;
+  onSubmit: () => void;
+  onCancel: () => void;
+}
+
+export const DeleteItemForm: React.FC<DeleteItemFormParams> = ({
+  itemName,
+  onSubmit,
+  onCancel,
+}) => {
+  return (
+    <div>
+      <h2>Are you sure you want to delete the {itemName}?</h2>
+
+      <div className="flex justify-end space-x-2">
+        <button
+          className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
+          onClick={onCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
+          onClick={onSubmit}
+        >
+          Yes
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/components/DeletePortfolioForm/index.tsx
+++ src/components/DeletePortfolioForm/index.tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "react-query";
 import { deletePortfolio } from "../../services/api/portfolios";
+import { DeleteItemForm } from "../Atomic/Molecules/DeleteItemForm";
 
 interface DeletePortfolioFormParams {
   portfolioId: string;
@@ -25,23 +26,10 @@ export const DeletePortfolioForm: React.FC<DeletePortfolioFormParams> = ({
   };
 
   return (
-    <div>
-      <h2>Are you sure you want to delete the portfolio?</h2>
-
-      <div className="flex justify-end space-x-2">
-        <button
-          className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
-          onClick={onCancel}
-        >
-          Cancel
-        </button>
-        <button
-          className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
-          onClick={handleSubmit}
-        >
-          Yes
-        </button>
-      </div>
-    </div>
+    <DeleteItemForm
+      itemName="portfolio"
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+    />
   );
 };
