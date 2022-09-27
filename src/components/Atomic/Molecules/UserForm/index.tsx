549b793352fe05dde76e7edabab4785af25b2387
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-27 12:02:22 -0300

    use form on UserForm

--- src/components/Atomic/Molecules/UserForm/index.tsx
+++ src/components/Atomic/Molecules/UserForm/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useState } from "react";
+import { ChangeEventHandler, FormEvent, useState } from "react";
 import { TUserCreate } from "services/api/users";
 import { Box } from "components/Atomic/Atoms/Box";
 
@@ -25,40 +25,43 @@ export const UserForm = ({
     setUser({ ...user, password: e.target.value });
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     onSubmit(user);
     setUser(USER_INITIAL);
   };
 
   return (
     <Box>
-      <h2 className="font-bold text-center">{title}</h2>
-      <div className="my-3">
-        <label>Username:</label>
-        <input
-          className="rounded-lg ml-3 p-2 border-slate-400 border-2"
-          type="text"
-          value={user.username}
-          onChange={onUsernameChange}
-        />
-      </div>
-      <div className="my-3">
-        <label>Password:</label>
-        <input
-          className="rounded-lg ml-3 p-2 border-slate-400 border-2"
-          type="password"
-          value={user.password}
-          onChange={onPasswordChange}
-        />
-      </div>
-      <div className="flex justify-end">
-        <button
-          className="bg-white m-3 p-1 px-5 rounded-lg border-cyan-500 border-2"
-          onClick={handleSubmit}
-        >
-          {submitButtonText}
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <h2 className="font-bold text-center">{title}</h2>
+        <div className="my-3">
+          <label>Username:</label>
+          <input
+            className="rounded-lg ml-3 p-2 border-slate-400 border-2"
+            type="text"
+            value={user.username}
+            onChange={onUsernameChange}
+          />
+        </div>
+        <div className="my-3">
+          <label>Password:</label>
+          <input
+            className="rounded-lg ml-3 p-2 border-slate-400 border-2"
+            type="password"
+            value={user.password}
+            onChange={onPasswordChange}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="bg-white m-3 p-1 px-5 rounded-lg border-cyan-500 border-2"
+            type="submit"
+          >
+            {submitButtonText}
+          </button>
+        </div>
+      </form>
     </Box>
   );
 };
