b3cb2000a478b6d479dbde71a49e617a824c884c
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-09-27 12:38:06 -0300

    create EditIcon component

--- src/components/Atomic/Organisms/AssetsList/index.tsx
+++ src/components/Atomic/Organisms/AssetsList/index.tsx
@@ -6,6 +6,7 @@ import { useModal } from "contexts/modalContext";
 import { fetchAssets, TAsset } from "services/api/assets";
 import { DeleteAssetForm } from "components/Atomic/Organisms/DeleteAssetForm";
 import { EditAssetForm } from "components/Atomic/Organisms/EditAssetForm";
+import { EditIcon } from "components/Atomic/Atoms/EditIcon";
 
 interface AssetsListParams {
   portfolioId: string;
@@ -67,19 +68,7 @@ export const AssetsList: React.FC<AssetsListParams> = ({ portfolioId }) => {
                 </td>
                 <td className="text-center space-x-3">
                   <button onClick={() => handleEditAsset(asset)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                      <path
-                        fillRule="evenodd"
-                        d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <EditIcon />
                   </button>
                   <button onClick={() => handleDeleteAsset(asset.id)}>
                     <svg
