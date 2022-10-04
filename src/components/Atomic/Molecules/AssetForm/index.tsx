ed747ed915847c1775a8a918c5e0268966ec3dc4
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-10-04 11:16:14 -0300

    validate AssetForm inputs

--- src/components/Atomic/Molecules/AssetForm/index.tsx
+++ src/components/Atomic/Molecules/AssetForm/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { TAssetBase } from "services/api/assets";
 
 interface AssetFormParams {
@@ -7,43 +7,40 @@ interface AssetFormParams {
   onSubmit: (asset: TAssetBase) => void;
 }
 
-const ASSET_INITIAL = { name: "" };
-
 export const AssetForm = ({
-  initialData = ASSET_INITIAL,
+  initialData,
   submitButtonText,
   onSubmit,
 }: AssetFormParams) => {
-  const [asset, setAsset] = useState<TAssetBase>(initialData);
+  const {
+    register,
+    handleSubmit: FormsHooksHandleSubmit,
+    formState: { errors },
+  } = useForm<TAssetBase>({ defaultValues: initialData });
 
-  const onNameChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setAsset({ ...asset, name: e.target.value });
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit: SubmitHandler<TAssetBase> = (asset) => {
     onSubmit(asset);
-    setAsset(initialData);
   };
 
   return (
-    <div>
+    <form onSubmit={FormsHooksHandleSubmit(handleSubmit)}>
       <div className="my-3 flex items-center">
-        <label>Name:</label>
+        <label>* Name:</label>
+        {errors.name && <span>This field is required</span>}
         <input
           className="rounded-lg ml-3 p-2 flex-grow border-slate-400 border-2"
           type="text"
-          value={asset.name}
-          onChange={onNameChange}
+          {...register("name", { required: true })}
         />
       </div>
       <div className="flex justify-end">
         <button
           className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
-          onClick={handleSubmit}
+          type="submit"
         >
           {submitButtonText}
         </button>
       </div>
-    </div>
+    </form>
   );
 };
