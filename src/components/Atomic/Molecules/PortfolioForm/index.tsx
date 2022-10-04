1be497dfff395d56de36de5cb78f14e209b99b94
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-10-04 11:12:52 -0300

    validate PortfolioForm inputs

--- src/components/Atomic/Molecules/PortfolioForm/index.tsx
+++ src/components/Atomic/Molecules/PortfolioForm/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { TPortfolioBase } from "services/api/portfolios";
 
 interface PortfolioFormParams {
@@ -7,43 +7,40 @@ interface PortfolioFormParams {
   onSubmit: (portfolio: TPortfolioBase) => void;
 }
 
-const PORTFOLIO_INITIAL = { name: "" };
-
 export const PortfolioForm = ({
-  initialData = PORTFOLIO_INITIAL,
+  initialData,
   submitButtonText,
   onSubmit,
 }: PortfolioFormParams) => {
-  const [portfolio, setPortfolio] = useState<TPortfolioBase>(initialData);
+  const {
+    register,
+    handleSubmit: FormsHooksHandleSubmit,
+    formState: { errors },
+  } = useForm<TPortfolioBase>({ defaultValues: initialData });
 
-  const onNameChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setPortfolio({ ...portfolio, name: e.target.value });
-  };
-
-  const handleSubmit = () => {
+  const handleSubmit: SubmitHandler<TPortfolioBase> = (portfolio) => {
     onSubmit(portfolio);
-    setPortfolio(initialData);
   };
 
   return (
-    <div>
+    <form onSubmit={FormsHooksHandleSubmit(handleSubmit)}>
       <div className="my-3 flex items-center">
-        <label>Name:</label>
+        <label>* Name:</label>
+        {errors.name && <span>This field is required</span>}
         <input
           className="rounded-lg ml-3 p-2 flex-grow border-slate-400 border-2"
           type="text"
-          value={portfolio.name}
-          onChange={onNameChange}
+          {...register("name", { required: true })}
         />
       </div>
       <div className="flex justify-end">
         <button
           className="bg-slate-100 my-3 p-1 px-5 rounded-2xl border-slate-500 border-2"
-          onClick={handleSubmit}
+          type="submit"
         >
           {submitButtonText}
         </button>
       </div>
-    </div>
+    </form>
   );
 };
