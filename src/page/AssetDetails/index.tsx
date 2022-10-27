829df4f5aa9608cc465c6f73c6a53008ce0a7209
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-10-26 22:52:19 -0300

    create back button on AssetDetails page

--- src/page/AssetDetails/index.tsx
+++ src/page/AssetDetails/index.tsx
@@ -1,12 +1,14 @@
+import { ArrowLeftIcon } from "@heroicons/react/20/solid";
 import { DepositsChart } from "components/Atomic/Molecules/DepositsChart";
 import { LoadingScreen } from "components/Atomic/Molecules/LoadingScreen";
 import { DepositsList } from "components/Atomic/Organisms/DepositsList";
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { fetchAsset } from "services/api/assets";
 
 export const AssetDetails = () => {
   const { assetId } = useParams();
+  const navigate = useNavigate();
 
   const query = useQuery(["asset", assetId], () =>
     assetId ? fetchAsset(assetId) : null
@@ -24,11 +26,20 @@ export const AssetDetails = () => {
     return null;
   }
 
+  const handleBackButton = () => {
+    navigate("/");
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-center text-3xl m-4">
-        {query.data.name.toLocaleUpperCase()} DEPOSITS
-      </h1>
+      <div className="grid grid-cols-3 w-full">
+        <button className="h-10 w-10 m-5" onClick={handleBackButton}>
+          <ArrowLeftIcon className="hover:fill-slate-400" />
+        </button>
+        <h1 className="text-center text-3xl m-4">
+          {query.data.name.toLocaleUpperCase()} DEPOSITS
+        </h1>
+      </div>
       <DepositsChart assetName={query.data.name} assetId={query.data.id} />
       <DepositsList assetId={query.data.id} />
     </div>
