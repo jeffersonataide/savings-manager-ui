f9b33010d3991bf6e1e1b32888a3b1cbaca57481
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2023-04-26 20:36:51 -0300

    update import from zustand

--- src/store/userStore.tsx
+++ src/store/userStore.tsx
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 import { getUserMe, TUserBase } from "@/services/api/users";
 import { getLocalJWT, removeLocalJWT, setLocalJWT } from "@/utils/localStorage";
 
